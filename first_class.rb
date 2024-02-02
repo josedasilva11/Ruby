f90356ed112e55b52ecdeaa11e4627763bb4a88a
José Pedro Silva <js.pdo.silv@gmail.com>
2024-02-02 09:43:25 +0000

    Create first_class.rb

---
+++ first_class.rb
@@ -0,0 +1,14 @@
+puts "Bem Vindos ao Ruby "
+# puts prints something in the terminal
+
+print "Outra forma de imprimir"
+# print doesn't use /n
+
+puts 100.0 + 10.0 * 2.0 / 3.0 - 2.0
+# numbers we don't use ""
+
+age = 29
+name = " Zé "
+actual_year = 2024
+puts age - actual_year
+# a variable holds a value
