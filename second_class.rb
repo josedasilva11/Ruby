fd96c144b078922bb6505046ad72eb135cc7d3a2
josedasilva11 <js.pdo.silv@gmail.com>
2024-02-06 16:39:56 +0100

    segunda aula adicionada

---
+++ second_class.rb
@@ -0,0 +1,15 @@
+# usar \ antes dumas aspas em strings vai imprimir as mesmas
+
+# integers - números inteiros
+puts 100
+puts -100
+
+# floats - números decimais
+puts 100.01
+puts 0.65333
+
+# operações aritméticas
+# PEMDAS - parentesis, expoentes, multiplicação, divisão, soma e subtração
+
+puts 10 % 5 # resto da divisão
+puts 10 ** 2 # 10 expoente 2
